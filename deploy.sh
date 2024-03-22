ff7e99715feaae24bae41e5a1838969011e89df5
alexandregoodmann <alexandregoodmann@gmail.com>
2024-03-22 19:00:31 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
 clear
+cd contabilidade-api
+sudo mvn clean install -DskipTests=true
+sleep 7
+cd ..
+cd contabilidade-web2
+sudo ng build
+sleep 7
+cd ..
 cd wordcard-api
 sudo mvn clean install -DskipTests=true
 sleep 7
